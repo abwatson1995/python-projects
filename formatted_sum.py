11d926f288e83a50734b526e195122a7f0f6ab8d
Arielle Watson <55118373+abwatson1995@users.noreply.github.com>
2023-11-12 12:45:00 -0500

    Add files via upload
    Week 3 - Formatted sums

---
+++ formatted_sum.py
@@ -0,0 +1,13 @@
+addend1 = int(input())
+addend2 = int(input())
+addend3 = int(input())
+
+sum = addend1 + addend2 + addend3
+
+print(f"{addend1:15d}")
+print(f"{addend2:15d}")
+print(f"{addend3:15d}")
+
+print('+ -------------')
+
+print(f"{sum:15d}")
